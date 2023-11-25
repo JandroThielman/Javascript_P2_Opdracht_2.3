d48e1f717f272ad71abba39ab42835f8c07953ed
JandroThielman <9020498@student.zadkine.nl>
2023-11-25 23:22:23 +0100

    jandro

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,10 @@
-setInterval(document.getElementById("Block").style.animation = "Move 2s linear infinite normal", 100);
+setInterval(document.getElementById("Block").style.animation = "Move 10s linear infinite normal", 100);
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById("Block").style.backgroundColor = "#" + randomColor;
+  }
+  
+  Block.addEventListener("click", setBg);
+  setBg();
+  
